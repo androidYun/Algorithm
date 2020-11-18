d94cec4d76e53356fef49789dbab890ceb85b94b
guiyun.li <xyz_6776@163.com>
2020-11-18 09:24:02 +0800

    两个数相加算æ³加发é打印æ多打印几个日志¥志add userInform 用户

---
+++ app/src/main/java/com/xhs/algorithm/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.xhs.algorithm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("打印日志")
+        println("打印日志")
+        println("打印日志")
+        println("打印日志")
+        println("打印日志")
+        println("打印日志")
+        println("打印日志")
+        println("打印日志")
+
+
+
+    }
+}
