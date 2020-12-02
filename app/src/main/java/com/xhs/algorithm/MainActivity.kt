d967370097fa30afd18124b1a866430de5834d6e
guiyun.li <xyz_6776@163.com>
2020-12-02 16:35:01 +0800

    A3

--- app/src/main/java/com/xhs/algorithm/MainActivity.kt
+++ app/src/main/java/com/xhs/algorithm/MainActivity.kt
@@ -19,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         println("打印日志")
         println("打印日志")
         println("打印日志")
+        println("打印日志")
+        println("打印日志")
         println("打印日志")1
         println("打印日志")2
         println("打印日志")2
